4b6fcfb45a6daedb1d3db039010a072ed4a1e629
devMal1 <devmal1.logic@gmail.com>
2016-05-07 12:43:35 -0400

    Added scripts to nscript dir; updated aliases path; added cdls script.

---
+++ nscripts/aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Push all to github: push <commit message> <branch>"
+alias push=". ~/Novelty-Scripts/nscripts/push_all.sh"
+
+echo "cd then ls: cdls <dir>"
+alias cdls=". ~/Novelty-Scripts/nscripts/cdls.sh"
