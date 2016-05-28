89375323b9e075819de9532c548b2c9cb8c76222
devMal1 <devmal1.logic@gmail.com>
2016-05-28 08:38:42 -0400

    Updated for user input on whether to continue, output looks cleaner.

--- nscripts/version_up.sh
+++ nscripts/version_up.sh
@@ -2,13 +2,34 @@
 
 
 #get commit id
-echo "getting latest commit id..."
-commit_id=$(git log --pretty=oneline -n 1 | egrep -o '[a-z0-9]{40}')
+echo "grabbing the latest commit..."
+latestLog="$(git log --pretty=oneline -n 1)"
+#commit_msg=$(echo "$latestLog" | egrep -o -v '[a-z0-p]{40}')
+#echo ">> $commit_msg"
+echo "$latestLog"
 echo
-echo "latest commit id => $commit_id"
 
-#up the version
-echo "upping the version..."
-git tag "$1" "$commit_id"
-echo
-echo "tag is now $1"
+echo "Up this tag? [y/n] > "
+read upThisTag
+if [ $upThisTag = 'y' ]; then
+	#up the version
+	commit_id=$(echo "$latestLog" | egrep -o '[a-z0-9]{40}')
+	echo
+	echo "latest commit id => $commit_id"
+
+	echo "upping the tag..."
+	git tag "$1" "$commit_id"
+	wasTagUpped=$?
+	echo
+
+	if [ $wasTagUpped != 0 ]; then
+		echo "tag was not upped."
+	else
+		echo "tag is now $1"
+	fi
+else
+	#do not up the version
+	#in future can add here to read in the specific commit id
+	echo
+	echo "Done."
+fi
