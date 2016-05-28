020c5ef5fe5686024fe027dc2fdcc34b9487ab53
devMal1 <devmal1.logic@gmail.com>
2016-05-28 07:45:06 -0400

    Updated the directories to use root variable and updated the echo and alias for version_up.

--- nscripts/aliases.sh
+++ nscripts/aliases.sh
@@ -3,14 +3,14 @@
 root="~/Novelty-Scripts/nscripts"
 
 echo "Push all to github: push <commit message> <branch>"
-alias push=". ~/Novelty-Scripts/nscripts/push_all.sh"
+alias push=". ${root}/push_all.sh"
 
 echo
 
 echo "cd then ls: cdls <dir>"
-alias cdls=". ~/Novelty-Scripts/nscripts/cdls.sh"
+alias cdls=". ${root}/cdls.sh"
 
 echo
 
-echo "Up the tag/version of latest commit"
-alias upTag=". {$root}/version_up.sh"
+echo "Up the tag/version of latest commit: uptag <tag#>"
+alias uptag=". ${root}/version_up.sh"
