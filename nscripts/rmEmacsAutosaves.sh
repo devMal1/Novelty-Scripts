1a4eb8b4c0b8846dca304bd0a84cf48c132d1ff3
Malik Malone <devmal1.logic@gmail.com>
2016-09-07 14:27:00 -0700

    Moved rmEmacs script into the nscript dir, and updated the script to exit the program if no autosave files are found within the dir.

---
+++ nscripts/rmEmacsAutosaves.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Check if there are autosave files to be deleted
+numOfFiles=$(ls | grep -c '~')
+if [ $numOfFiles -gt 0 ]; then
+    #Display files that will be deleted in bulk
+    echo "Files to be deleted:"
+    ls | grep '~'
+
+    #Confirm delete all
+    echo "Delete all? (yes/no) : "
+    read delete
+
+    if [ $delete = 'yes' ]; then
+        #Create backup (just in case)
+        nowTime=$(date +%H_%M_%S)
+        backupDir="backup${nowTime}"
+        mkdir "$backupDir"
+        cp ./* ./"$backupDir" |
+
+        #Delete files
+        echo #new line
+        for autoFile in `ls | grep '~'`; do
+    	echo "deleting ${autoFile}..."
+    	rm $autoFile
+        done
+        echo "deleted."
+
+        #Delete backup?
+        echo
+        echo "Folder content:"
+        ls ./
+        echo "Does everything look good.. shall we delete the backup dir? (yes/no) : "
+        read deleteBackup
+        if [ $deleteBackup = 'yes' ]; then
+    	echo
+    	echo "Removing backup dir..."
+    	rm -r "$backupDir"
+    	echo "done and done."
+        fi
+    else
+        echo
+        echo "No files were deleted."
+    fi
+else
+    echo "No autosave files to be deleted."
+fi
