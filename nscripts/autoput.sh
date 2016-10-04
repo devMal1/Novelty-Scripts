a7764dcbc3e46d820ece43eabb6e9734cf28e75d
Malik Malone <devMal1.logic@gmail.com>
2016-10-03 22:09:50 -0700

    Fully implemented, but having some major bugs at the moment :(

--- nscripts/autoput.sh
+++ nscripts/autoput.sh
@@ -1,67 +1,127 @@
 #!/bin/bash
 
 #Will monitor a file and ftp-put it on a remote server
+#Globals
+CHECK_CODE=255
+GET_ORIGINAL_TIME=2
+GET_REMOTE_DIR=3
+TIME_PATTERN="[0-9]{1,2}:{1}[0-9]{2}"
 
 #Helper functions
 function getLastModifiedTime {
 	local _file=$1
-	echo "$( ls -l $_file | egrep -o "[0-9]{1,2}:{1}[0-9]{2}" )"
+	echo "$( ls -l $_file | egrep -o "$TIME_PATTERN" )"
 }
 function updateLastModifiedTime {
 	local _file=$1
-	local _newTime=$2
+	local _oldTime=$2
+	local _newTime=$3
+	local _listOfFiles=$4
+	
+	local _searchFor="$_file $oldTime"
+	local _replaceWith="$_file $newTime"
+	local _temp="temp.txt"
+	touch $_temp
+	sed "s|$_searchFor|$_replaceWith|" $_listOfFiles >$_temp
+	#rm $_listOfFiles
+	#mv $_temp $_listOfFiles
+}
+function queryData {
+	local _file=$1
+	local _column=$2
 	local _listOfFiles=$3
-	echo "updateLastModifiedTime"
+
+	if [ $_column = 2 ]; then
+		echo "$( awk -v f=$_file '$1=$f {print $2}' $_listOfFiles )"
+	elif [ $_column = 3 ]; then
+		echo "$( awk -v f=$_file '$1=$f {print $3}' $_listOfFiles )"
+	else
+		echo $CHECK_CODE
+	fi
 }
 
 #Setup (get file paths from user)
 function getFilesInfoFromUser {
-	local $_listOfFiles=$1
-	echo "Enter full paths (do not use '~') for the files you would like to autoput (one file path per line):"
+	local _listOfFiles=$1
+	echo "Enter full paths (do not use '~') for the files you would like to autoput (one file path per line)"
+	echo "For each file you will be prompted for the full path remote directory for that file (where the file will be put on the server)"
 	echo "Type 'done' when finished."
 	echo "" > $_listOfFiles #clear list
 	local _isDone=0
 	while [ $_isDone = 0 ]; do
+		local _file=""
 		read _file
 		if [ $_file = "done" ]; then
 			_isDone=1
 		else
 			if [ -e $_file ]; then
-				echo "adding $_file ..."
+				echo "Remote Directory for ${_file}:"
+				local _remoteDir=""
+				read _remoteDir
 				local _timeLastModified="$( getLastModifiedTime $_file )"
-				echo "$_file  $_timeLastModified" >> $_listOfFiles
+				echo "$_file  $_timeLastModified $_remoteDir" >> $_listOfFiles
+				echo "$_file added."
 			else 
 				echo "WARNING: File does not exist, ignoring file (make sure using beginning '/')"
 			fi
 		fi
 	done
 }
+function getSFTPInfoFromUser {
+	local _user=$1
+	
+	echo "Enter host name for remote server:"
+	local _server=""
+	read _server
+	
+	echo "Enter user name for ${server}:"
+	local _username=""
+	read _username
+	
+	echo "${_username}@${_server}" > $_user
+}
 
 #File status
 function hasFileChanged {
 	local _file=$1
 	local _listOfFiles=$2
+	local _TIME=2
 
-	local _originalTimeLastModified="$( awk -v f=$_file '$1=$f {print $2}' $_listOfFiles )" 
+	local _originalTimeLastModified="$( queryData $_file $GET_ORIGINAL_TIME $_listOfFiles )" 
 	local _timeLastModified="$( getLastModifiedTime $_file )"
+	echo "DEBUG::: $_originalTimeLastModified :: $_timeLastModified"
+	echo "something!!!"
 	if [ "$_originalTimeLastModified" = "$_timeLastModified" ]; then
-		echo 0
+		echo $CHECK_CODE
 	else
+		updateLastModifiedTime $_file $_originalTimeLastModifed $_timeLastModified $_listOfFiles
 		echo "$_timeLastModified"
 	fi
 }
 
 #ftp-put file to remote server
-function ftpPut {
+function sftpPut {
 	local _file=$1
-	echo "ftpPut"	
+	local _remoteDir=$2
+	local _user="$3"
+	
+	local _sftPutFile="sftpPut.txt"
+	echo "cd $_remoteDir" > $_sftPutFile
+	echo "put $_file" >> $_sftPutFile
+	local _userAtHost="$( cat $_user )"
+	sftp -b "$_sftpPutFile" "$userAtHost"
 }
 
 
 #Main
 function monitorFiles {
+	local _user=".user"
 	local _listOfFiles="monitoring.txt"
-	
+
+	#check if $user already exists
+	###getSFTPInfoFromUser $_user
+
+	#check if user wants to use previous files.. print out files then ask
 	getFilesInfoFromUser $_listOfFiles
 
 	echo "Monitoring the files..."
@@ -70,14 +130,19 @@ function monitorFiles {
 	while [ $_stop = 0 ]; do
 		#Monitor files
 		for file in $( awk '{print $1}' $_listOfFiles ); do
-			local _newTime=$( hasFileChanged "$file" "$_listOfFiles" )
-			if [ "$_newTime" != 0 ]; then
-				echo "$_file changed"
-				updateLastModifiedTime $file $_newTime $_listOfFiles
-				ftpPut $file
+			local _changed=$( hasFileChanged "$file" "$_listOfFiles" )
+			if [ "$_changed" = $CHECK_CODE ]; then
+				echo "$file changed"
+				#local _remoteDir=$( queryData $file $GET_REMOTE_DIR $_listOfFiles )
+				##sftpPut $file $_remoteDir $_user
 			fi
 		done
 	done
 }
 
+#Run Main
 monitorFiles
+
+
+#reference to add to the readme.. ssh keys
+#www.computerhope.com/unix/sftp.htm
